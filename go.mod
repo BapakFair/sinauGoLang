6f712069384387ab30b5ad3f3c5ffe33b55a11ab
bapakfair <fairsulaiman@gmail.com>
2022-01-11 13:55:45 +0700

    sinau babak pertama

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sinauGoLang
+
+go 1.17
---
+++ sinau.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Fair!")
+}
+
+//type data, alias :
+//	byte = uint8
+//	rune = int32
+//	int = minimal int32
+//	uint = minimal uin32
